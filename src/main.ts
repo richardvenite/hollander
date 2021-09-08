88b6000e1daccd3f6000c08aefd592c48066e2ec
Richard Venite <richard.miguez@gmail.com>
2021-09-08 09:12:33 -0300

    install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
@@ -6,8 +7,19 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-
+  
   const port = process.env.PORT
+  
+  const config = new DocumentBuilder()
+    .setTitle('Hollander')
+    .setDescription('Hollander API description')
+    .setVersion('1.0')
+    .addTag('hollander')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(port);
 
   logger.log(`Application listening on port ${port}`);
